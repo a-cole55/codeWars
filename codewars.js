20aa13bf5a576f4671fba9c53fcfe3cac0921c55
a-cole55 <arniqua_c@yahoo.com>
2022-03-27 09:59:32 -0500

    Added Daily Challenge Day 2

--- codewars.js
+++ codewars.js
@@ -29,3 +29,38 @@ function numSquared(n){
     console.log(stringSquared);
 }
 numSquared(9119)
+
+//Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+//Note: a and b are not ordered!
+
+//Examples (a, b) --> output (explanation)
+//(1, 0) --> 1 (1 + 0 = 1)
+//(1, 2) --> 3 (1 + 2 = 3)
+
+function getSum( a,b )
+{
+   a = (a);
+   b = (b);
+  const arr = [];
+   //Good luck!
+  if (a === b){
+    return a;
+  } else if (a > b){
+    let i = b;
+    while (i <= a){
+      arr.push(i);
+      (i) += 1;
+       } 
+    let sum = arr.reduce((acc, cv) => acc + cv);
+    return sum;
+   } else {
+    let i = a;
+    while (i <= b){
+      arr.push(i);
+      (i) += 1;
+       } 
+    let sum = arr.reduce((acc, cv) => (acc) + (cv));
+    return sum
+   }
+}
