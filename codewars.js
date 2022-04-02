653cab4bbc8b08ec97b719cb797a2b50330d8bad
a-cole55 <arniqua_c@yahoo.com>
2022-04-02 11:19:48 -0500

    daily code challenge

--- codewars.js
+++ codewars.js
@@ -149,3 +149,49 @@ XO("xxOo");
 XO("xxxm");
 XO("Oo");
 XO("ooom");
+
+
+//Make a program that filters a list of strings and returns a list with only your friends name in it.
+//If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+//Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+//i.e.
+//friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+//Note: keep the original order of the names in the output.
+function friend(friends){
+  //your code here
+  let realFriends = friends.filter(friend => friend.length === 4);
+  return realFriends
+}
+friend(["Ryan", "Kieran", "Mark"])
+friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])
+
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, 
+//are named with letters from a to m.
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning 
+//that the printer used three times color a, four times color b, one time color h then one time color a...
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm 
+//with letters not from a to m.
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a 
+//rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+// The string has a length greater or equal to one and contains only letters from ato z.
+// Examples:
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+function printerError(s) {
+  // your code
+  s = s.toLowerCase()
+  const regex = /[a-m]/g;
+  const correct = s.match(regex).length;
+  let errors = s.length - correct
+  return `${errors}/${s.length}`
+}
+function printerError(s) {
+  // your code
+  const regex = /[a-mA-M]/g;
+  const correct = s.match(regex).length;
+  return `${s.length - correct}/${s.length}`
+}
