90d9d909ea4e39f26d20f7db41d1dfb65c4bd7a3
a-cole55 <arniqua_c@yahoo.com>
2022-04-17 12:07:14 -0500

    daily js challenge

---
+++ 8kyu-Reversed sequence.js
@@ -0,0 +1,13 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+  let arr = []
+  for(let i = 0; n > i; n--){
+    arr.push(n)
+  }
+  return arr
+};
+
+reverseSeq(5) //([5, 4, 3, 2, 1])
