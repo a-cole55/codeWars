d0fd78e5439962e76ccefb08cc8f87375422f5e8
a-cole55 <arniqua_c@yahoo.com>
2022-04-22 15:24:13 -0500

    daily python challenge

---
+++ Python/8kyu-Sentence Smash.py
@@ -0,0 +1,30 @@
+# Sentence Smash
+# Write a function that takes an array of words and smashes them together into a sentence 
+# and returns the sentence. You can ignore any need to sanitize words or add punctuation, 
+# but you should add spaces between each word. Be careful, there shouldn't be a space at the 
+# beginning or the end of the sentence!
+
+# Example
+# ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+def smash(words):
+    if len(words) == 0:
+        return ''
+    elif len(words) == 1:
+        return words[0]
+    else:
+        return ' '.join(words)
+
+
+smash([]) 
+#("")
+        
+smash(["hello"])
+#("hello")
+        
+smash(["hello", "world"])
+#("hello world")
+smash(["hello", "amazing", "world"])
+#("hello amazing world")
+smash(["this", "is", "a", "really", "long", "sentence"])
+#("this is a really long sentence")
