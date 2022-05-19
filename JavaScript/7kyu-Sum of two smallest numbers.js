13d88c1cb0a72f308a8a1050e74650fb400d6f0e
a-cole55 <arniqua_c@yahoo.com>
2022-05-19 12:16:39 -0500

    daily js challenge

---
+++ JavaScript/7kyu-Sum of two smallest numbers.js
@@ -0,0 +1,19 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+  let sorted = numbers.sort((a,b) => a-b)
+  return sorted[0] + sorted[1]
+}
+
+sumTwoSmallestNumbers([5, 8, 12, 19, 22]) //13
+sumTwoSmallestNumbers([15, 28, 4, 2, 43]) //6
+sumTwoSmallestNumbers([3, 87, 45, 12, 7]) //10
+sumTwoSmallestNumbers([23, 71, 33, 82, 1]) //24
+sumTwoSmallestNumbers([52, 76, 14, 12, 4]) //16
+
