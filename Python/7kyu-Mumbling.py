2157f0517cf0ada9acae7574d53472b63f02fa1d
a-cole55 <arniqua_c@yahoo.com>
2022-05-23 21:13:32 -0500

    daily python challenge

---
+++ Python/7kyu-Mumbling.py
@@ -0,0 +1,26 @@
+# This time no story, no theory. The examples below show you how to write function accum:
+
+# Examples:
+# accum("abcd") -> "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt") -> "C-Ww-Aaa-Tttt"
+# The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+def accum(s):
+    # your code
+    mumble = ''
+    i = 0
+    for str in s:
+        if str == str.upper():
+            max = str + (str.lower() * i)
+        else:
+            max = str.upper() + (str * i)
+        mumble += max + '-'
+        i += 1
+    return mumble[:-1]
+
+accum("ZpglnRxqenU") #"Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+accum("NyffsGeyylB") #"N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+accum("MjtkuBovqrU") #"M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+accum("EvidjUnokmM") #"E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+accum("HbideVbxncC") #"H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
