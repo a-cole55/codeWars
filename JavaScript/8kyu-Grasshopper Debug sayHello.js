d7f44daf77b24dc5d8f9929071c83a94d1f713dd
a-cole55 <arniqua_c@yahoo.com>
2022-05-24 14:29:39 -0500

    daily js challenge

---
+++ JavaScript/8kyu-Grasshopper Debug sayHello.js
@@ -0,0 +1,15 @@
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet 
+// everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+function sayHello(name) {
+  return 'Hello'.concat(`, ${name}`)
+}
+sayHello('Mr. Spock') //'Hello, Mr. Spock')
+sayHello('Captain Kirk') //'Hello, Captain Kirk')
+sayHello('Liutenant Uhura') //'Hello, Liutenant Uhura')
+sayHello('Dr. McCoy') //'Hello, Dr. McCoy')
