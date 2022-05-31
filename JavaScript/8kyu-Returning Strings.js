03340644b83f778a729695e96ef00ad4a48bb9c6
a-cole55 <arniqua_c@yahoo.com>
2022-05-31 18:08:42 -0500

    daily js challenge 2

---
+++ JavaScript/8kyu-Returning Strings.js
@@ -0,0 +1,12 @@
+// Make a function that will return a greeting statement that uses an input; your program 
+// should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+  //your code here
+  return `Hello, ${name} how are you doing today?`
+}
+
+greet("Ryan") //"Hello, Ryan how are you doing today?");
+greet("Shingles") //"Hello, Shingles how are you doing today?");
