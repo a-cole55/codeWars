8d36e7a32e685180c10f63f808de7d086f066db1
a-cole55 <arniqua_c@yahoo.com>
2022-06-14 10:44:23 -0500

    daily python challenge

---
+++ Python/8kyu-Basic Function Fixer.py
@@ -0,0 +1,10 @@
+# Fix the function
+# I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+# Can you help me fix the function?
+
+def add_five(num):
+    return num + 5
+add_five(5) #10)
+add_five(0) #5)
+add_five(-5) #0)
