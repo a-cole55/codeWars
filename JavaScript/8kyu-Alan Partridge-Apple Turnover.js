9b991364b0e4cb85927a7f53bcf723e204eb29c1
a-cole55 <arniqua_c@yahoo.com>
2022-06-18 18:40:33 -0500

    daily js challenge

---
+++ JavaScript/8kyu-Alan Partridge-Apple Turnover.js
@@ -0,0 +1,11 @@
+// Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+
+// X will be a valid integer number.
+
+
+function apple(x){
+    return x**2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
+
+apple('50'), 'It\'s hotter than the sun!!'); 
+apple(4), 'Help yourself to a honeycomb Yorkie for the glovebox.'); 
