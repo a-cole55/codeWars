d7e02278c41d95d631472cc1ffd7a31f4bc2b010
a-cole55 <arniqua_c@yahoo.com>
2022-08-23 21:16:41 -0500

    daily js challenge

---
+++ JavaScript/8kyu-Function 2-squaring an argument.js
@@ -0,0 +1,8 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+function square(x){
+    return x**2
+  }
+
+  square(3) //9)
