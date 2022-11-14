2cafa2f128fd4a9fb2b2eb5dd676e12aba0fb02a
a-cole55 <arniqua_c@yahoo.com>
2022-11-14 17:19:57 -0600

    daily coding challenge

---
+++ JavaScript/7kyu-Find the Middle Number.js
@@ -0,0 +1,22 @@
+// You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers.
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+
+function gimme(arr){
+    let sortedCopy = [...arr].sort((a,b) => a - b);
+    let midVal = sortedCopy[1];
+    return arr.indexOf(midVal)
+  }
+  gimme([2, 3, 1]) //=> 0
+  gimme([5, 10, 14]) //=> 1
